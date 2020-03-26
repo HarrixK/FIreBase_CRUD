eccb7a4f83b1923782143b66719fb620e226bd3a
Haris Ullah Khan <harrixkhan5515@gmail.com>
2020-03-26 14:09:19 +0500

    CRUD Operations on Firebase CloudStorage

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     //Step 1: Create object of Firebase Firestore
     private FirebaseFirestore objectFirebaseFirestore;
     private Dialog objectDialog;
+
+    private static final String CollectionName = "NewCities";
 
     private EditText documentET, cityNameET, cityDetailsET;
     private TextView valuetextbox;
@@ -49,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         cityNameET = findViewById(R.id.CityName);
 
         cityDetailsET = findViewById(R.id.DetailsET);
-        valuetextbox=findViewById(R.id.GetDataTV);
+        valuetextbox = findViewById(R.id.GetDataTV);
 
         objectDialog.setContentView(R.layout.please_wait_layout);
     }
@@ -68,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                 objectMap.put("city_name", cityNameET.getText().toString());
 
                 objectMap.put("city_details", cityDetailsET.getText().toString());
-                objectFirebaseFirestore.collection("NewCities")
+                objectFirebaseFirestore.collection(CollectionName)
                         .document(documentET.getText().toString()).set(objectMap)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
@@ -95,15 +98,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void getData(View view) {
         try {
+            objectDialog.show();
             if (!documentET.getText().toString().isEmpty()) {
-                objectDocumentReference = objectFirebaseFirestore.collection("NewCities").document(documentET.getText().toString()
-                );
+                objectDocumentReference = objectFirebaseFirestore.collection("NewCities").
+                        document(documentET.getText().toString());
                 objectDocumentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        String City = documentSnapshot.getString("city_name");
-                        String Details = documentSnapshot.getString("city_details");
-                        valuetextbox.setText("\nCity Name: " + City + "\n\n" + "City Details: " + Details);
+                        objectDialog.dismiss();
+                        if (documentSnapshot.exists()) {
+                            valuetextbox.setText("");
+                            documentET.setText("");
+
+                            documentET.requestFocus();
+                            String City = documentSnapshot.getString("city_name");
+                            String Details = documentSnapshot.getString("city_details");
+                            valuetextbox.setText("\nCity Name: " + City + "\n\n" + "City Details: " + Details);
+                        } else {
+                            Toast.makeText(MainActivity.this, "No Documents Retrieved", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
@@ -114,6 +127,60 @@ public class MainActivity extends AppCompatActivity {
             }
         } catch (Exception ex) {
             Toast.makeText(this, "Get Data Error" + ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void update(View V){
+        try{
+            objectDialog.show();
+            objectDocumentReference=objectFirebaseFirestore.collection(CollectionName).document(documentET.getText().toString());
+            Map<String, Object> objectMap=new HashMap<>();
+            objectMap.put("city_name", cityNameET.getText().toString());
+            objectMap.put("city_details", cityDetailsET.getText().toString());
+
+            objectDocumentReference.update(objectMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    objectDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Data Updated Successfully", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    objectDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Data Failed To Updated" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        catch(Exception ex){
+            Toast.makeText(this, "Update Error: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void Remove(View V){
+        try{
+            objectDialog.show();
+            objectDocumentReference=objectFirebaseFirestore.collection(CollectionName).document(documentET.getText().toString());
+            Map<String, Object> objectMap=new HashMap<>();
+//            objectMap.put("city_name", FieldValue.delete());
+//            objectMap.put("city_details", FieldValue.delete());
+
+            objectDocumentReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    objectDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Data Deleted", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    objectDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Data Failed To Delete" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        catch(Exception ex){
+            Toast.makeText(this, "Update Error: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 }
